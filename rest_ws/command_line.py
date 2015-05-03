3e388e48b2419429d15e945fc77b1a4eee2a8755
Valeryi Savich <Relrin78@gmail.com>
2015-05-03 17:57:36 +0300

    Tests for command_line module

--- rest_ws/command_line.py
+++ rest_ws/command_line.py
@@ -3,15 +3,15 @@ from argparse import ArgumentParser
 options = ArgumentParser()
 
 
-def define(name, default=None, help=None, type=None, required=True):
+def define(name, default=None, help=None, type=None, *args, **kwargs):
     """
         Defines an option in the global namespace
     """
     return options.add_argument(name, default=default, help=help, type=type,
-                                required=required)
+                                *args, **kwargs)
 
 
-def parse_command_line():
+def parse_command_line(args=None):
     """
         Parse options from the command line
     """
---
+++ tests/test_command_line.py
@@ -0,0 +1,26 @@
+from rest_ws.command_line import options, define, parse_command_line
+from fixtures.cmd import default_options
+
+
+def test_define(default_options):
+    assert len(options._option_string_actions.keys()) == 4
+
+
+def test_define_with_user_arguments(default_options):
+    define('-arg1', default='127.0.0.1', help='used ip for server', type=str)
+    define('-arg2', default=8080, help='listened on server', type=int)
+    assert len(options._option_string_actions.keys()) == 6
+
+
+def test_define_with_user_arguments_and_options(default_options):
+    define('-arg1', default='127.0.0.1', help='used ip for server', type=str)
+    define('-arg2', default=8080, help='listened on server', type=int)
+    define('--arg1', help='arg #1', type=int)
+    define('--arg2', help='arg #2', type=str)
+    assert len(options._option_string_actions.keys()) == 8
+
+
+def test_parse_command_line(default_options):
+    res = parse_command_line()
+    assert res.ip == '127.0.0.1'
+    assert res.port == 8080
