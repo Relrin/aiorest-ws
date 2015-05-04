186098720b934be22295e17486fa8f2e8692e595
Valeryi Savich <Relrin78@gmail.com>
2015-05-04 23:26:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('flask/__init__.py', 'rb') as f:
+with open('aiorest_ws/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
