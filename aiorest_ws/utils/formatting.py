27e623bd5a4677d9df3aca9f6fdbbe066d0a4cbb
Valeryi Savich <Relrin78@gmail.com>
2015-07-12 15:01:22 +0300

    JSON/XML serializers

---
+++ aiorest_ws/utils/formatting.py
@@ -0,0 +1,10 @@
+# -*- coding: utf -*-
+
+# JSON serializer options
+SHORT_SEPARATORS = (',', ':')
+LONG_SEPARATORS = (', ', ': ')
+
+WRONG_UNICODE_SYMBOLS = [
+    ("\u2028", "\\u2028"),
+    ("\u2029", "\\u2029"),
+]
