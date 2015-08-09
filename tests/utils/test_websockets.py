de22c6939fdaed166dc939c72f6ac9b459f41c66
Valeryi Savich <Relrin78@gmail.com>
2015-08-09 20:02:45 +0300

    Fixed message compressing tests

--- tests/utils/test_websockets.py
+++ tests/utils/test_websockets.py
@@ -9,7 +9,8 @@ from aiorest_ws.utils.websocket import deflate_offer_accept
 
 @pytest.mark.parametrize("offers, expected", [
     ([PerMessageDeflateOffer(), ], PerMessageDeflateOfferAccept),
-    ([None, ], type(None))
+    ([None, ], type(None)),
+    ([], type(None)),
 ])
 def test_deflate_offer_accept(offers, expected):
     assert type(deflate_offer_accept(offers)) is expected
