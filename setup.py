1e02d9082e42ba9bd9f88b6cbe20c30e13f1002a
Valeryi Savich <Relrin78@gmail.com>
2015-12-02 23:58:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ args = dict(
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP'
     ],
