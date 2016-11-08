b2c29f132e56fb176a2b20525789588a549a9fe3
Valeryi Savich <Relrin78@gmail.com>
2016-11-08 22:10:51 +0300

    Added tests for compat module

--- aiorest_ws/db/orm/django/compat.py
+++ aiorest_ws/db/orm/django/compat.py
@@ -5,11 +5,13 @@ versions of Django, and compatibility wrappers around optional packages.
 """
 import inspect
 
-import django
-
 from django.apps import apps
 from django.core.exceptions import ImproperlyConfigured
 from django.db import models
+
+from django.core.validators import DecimalValidator
+from django.contrib.postgres import fields as postgres_fields
+from django.contrib.postgres.fields import JSONField
 
 
 __all__ = [
@@ -17,24 +19,6 @@ __all__ = [
     '_resolve_model', 'get_related_model', 'get_remote_field',
     'value_from_object'
 ]
-
-
-try:
-    from django.core.validators import DecimalValidator
-except ImportError:
-    DecimalValidator = None
-
-
-try:
-    from django.contrib.postgres import fields as postgres_fields
-except ImportError:
-    postgres_fields = None
-
-
-try:
-    from django.contrib.postgres.fields import JSONField
-except ImportError:
-    JSONField = None
 
 
 def _resolve_model(obj):
@@ -60,16 +44,10 @@ def _resolve_model(obj):
 
 
 def get_related_model(field):
-    if django.VERSION < (1, 9):
-        return _resolve_model(field.rel.to)
     return field.remote_field.model
 
 
-# field.rel is deprecated from 1.9 onwards
 def get_remote_field(field, **kwargs):
-    if 'default' in kwargs:
-        return getattr(field, 'remote_field', kwargs['default'])
-
     return field.remote_field
 
 
---
+++ tests/db/orm/django/test_compat.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+from unittest.mock import patch
+
+from django.core.exceptions import ImproperlyConfigured
+from django.db import models
+
+from aiorest_ws.db.orm.django.compat import _resolve_model, \
+    get_related_model, get_remote_field, value_from_object
+
+from tests.db.orm.django.base import DjangoUnitTest
+
+
+class CustomStringField(models.CharField):
+    pass
+
+
+class TestDjangoCompatModule(DjangoUnitTest):
+
+    class Manufacturer(models.Model):
+        name = CustomStringField(max_length=30)
+
+        class Meta:
+            app_label = 'test_django_compat_module'
+
+        def __str__(self):
+            return '<User(%s)>' % self.name
+
+    class Car(models.Model):
+        name = models.CharField(max_length=30)
+        max_speed = models.FloatField(null=True, blank=True)
+        manufacturer = models.ForeignKey(
+            "test_django_compat_module.Manufacturer", related_name='cars',
+            null=True, blank=True
+        )
+
+        class Meta:
+            app_label = 'test_django_compat_module'
+
+        def __str__(self):
+            return '<Car(%s, %s)>' % (self.name, self.manufacturer)
+
+    apps = ('test_django_compat_module', )
+    models = (Manufacturer, Car)
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestDjangoCompatModule, cls).setUpClass()
+        user = cls.Manufacturer.objects.create(name='TestName')
+        car = cls.Car.objects.create(name='TestCar', max_speed=350)
+        user.cars.add(car)
+        user.save()
+
+    def test_resolve_model_return_object(self):
+        self.assertEqual(_resolve_model(self.Manufacturer), self.Manufacturer)
+
+    def test_resolve_model_return_object_by_model_as_string(self):
+
+        class FakeCarConfig(object):
+
+            model_mapping = {
+                'car': self.Car
+            }
+
+            def get_model(config, model_name):
+                return config.model_mapping.get(model_name, None)
+
+        patched_module = 'django.apps.apps.app_configs'
+        patched_value = {'test_django_compat_module': FakeCarConfig()}
+        with patch(patched_module, new=patched_value):
+            value = "test_django_compat_module.car"
+            self.assertEqual(_resolve_model(value), self.Car)
+
+    def test_resolve_model_raises_improperly_configured_exception(self):
+
+        class FakeConfig(object):
+
+            def get_model(self, model_name):
+                return None
+
+        patched_module = 'django.apps.apps.app_configs'
+        patched_value = {'invalid_app': FakeConfig()}
+        with patch(patched_module, new=patched_value):
+            value = "invalid_app.fake_model"
+            self.assertRaises(ImproperlyConfigured, _resolve_model, value)
+
+    def test_resolve_model_raises_value_error_exception(self):
+
+        class NotDjangoModel(object):
+            pass
+
+        obj = NotDjangoModel()
+        self.assertRaises(ValueError, _resolve_model, obj)
+
+    def test_get_related_model(self):
+        field = self.Car._meta.get_field('manufacturer')
+        self.assertEqual(get_related_model(field), field.remote_field.model)
+
+    def test_get_remote_field(self):
+        field = self.Car._meta.get_field('manufacturer')
+        self.assertEqual(get_remote_field(field), field.remote_field)
+
+    def test_value_from_object(self):
+        field = self.Manufacturer._meta.get_field('name')
+        obj = self.Manufacturer.objects.get(name='TestName')
+        self.assertEqual(value_from_object(field, obj), obj.name)
