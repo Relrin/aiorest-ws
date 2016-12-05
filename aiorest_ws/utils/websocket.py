8667df2a43cddef3e93f09d0a595cd7510a735c0
Valeryi Savich <Relrin78@gmail.com>
2016-12-05 14:03:01 +0300

    Update websocket.py

--- aiorest_ws/utils/websocket.py
+++ aiorest_ws/utils/websocket.py
@@ -12,7 +12,8 @@ __all__ = ('deflate_offer_accept', )
 def deflate_offer_accept(offers):
     """
     Function to accept offers from the client.
-    NOTE: For using this you will need a "permessage-deflate" extension.
+    NOTE: For using this function you will need a "permessage-deflate" 
+    compression extension for WebSocket connections.
 
     :param offers: iterable object (list, tuple), where every object
                    is instance of PerMessageDeflateOffer.
